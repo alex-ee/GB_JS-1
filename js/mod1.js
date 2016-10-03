b050a00c12d127c06f0da2068ad2853396f40ce7
Alexey <alexey.gatchina@gmail.com>
2016-10-03 21:26:09 +0300

    дз4

--- js/mod1.js
+++ js/mod1.js
@@ -1,8 +1,8 @@
 "use strict";
 
-dz2_3();
-dz3_1();
-dz3_2();
+dz4_1();
+dz4_2();
+dz4_3();
 
 function dz1() {
     var admin, name = "Василий";
@@ -46,22 +46,12 @@ function dz2_3() {
     for (var i = 1; i <= 100; i++) {
         // Fizz  Buzz
 
-        console.log (((i % 3 == 0) && (i % 5 == 0)) ? "" + i + "   FizzBuzz" :
-            (i % 3 == 0) ? "" + i + "   Fizz" :
-                (i % 5 == 0) ? "" + i + "   Buzz" :
-                    i
+        console.log (
+            ((i % 3 == 0) && (i % 5 == 0)) ? "" + i + "   FizzBuzz" :
+                (i % 3 == 0) ? "" + i + "   Fizz" :
+                    (i % 5 == 0) ? "" + i + "   Buzz" :
+                        i
         );
-/*
-        if ((i % 3 == 0) && (i % 5 == 0)) {
-            console.log ("" + i + "   FizzBuzz");
-        } else if (i % 3 == 0) {
-            console.log ("" + i + "   Fizz");
-        } else if (i % 5 == 0) {
-            console.log ("" + i + "   Buzz");
-        } else {
-            console.log (i);
-        }
-*/
     }
 }
 
@@ -85,4 +75,69 @@ function dz3_2() {
     }
     
     console.log (ret);
+}
+
+function dz4_1() {
+    function min() {
+        return (arguments[0] <= arguments[1]) ? arguments[0] : arguments[1];
+    }
+
+    console.log (min(11, 11));
+    console.log (min(0, 10));
+    console.log (min(0, -10));
+}
+
+function dz4_2() {
+    function countChr (str, chr) {
+        var i = 0, str = '' + str;
+
+        if (str.length > 1) {
+            i = str.charAt (0) === chr ? 1 : 0;
+            i += countChr (str.substring (1), chr);
+
+        } else {
+            i = str.charAt (0) === chr ? 1 : 0;
+
+        }
+
+        return  i;         
+    }
+
+    console.log (
+        'буква "t" в строке "Test test test" встречается ' + countChr ('Test test test','t') + " раз(а)"
+    );
+
+    console.log (
+        'буква "e" в строке "Test test test" встречается ' + countChr ('Test test test','e') + " раз(а)"
+    );
+}
+
+function dz4_3() {
+    function isEven (int) {
+        var bool = false;
+
+        int = Math.abs(int);
+
+        if (int > 1) {
+            bool = isEven (int - 2);
+
+        } else {
+            bool = (int === 1) ? false : true; 
+
+        }
+
+        return bool;
+    }
+
+    console.log (
+        'чётность 50-и = ' + isEven (50)    
+    );
+
+    console.log (
+        'чётность 75-и = ' + isEven (75)    
+    );
+
+    console.log (
+        'чётность (-1) = ' + isEven (-1) + ' (фикс в том чтобы брать по модулю)'
+    );
 }
